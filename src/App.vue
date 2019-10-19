3332c9af0ac28255d37ee3a934597d557df46d49
Ben Winding <ben.winding@gmail.com>
2019-10-20 10:12:21 +1030

    Slider range v-model working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mx-auto p-4">
+    <range-input v-model="value"></range-input>
+    <p>Wealth Visualisation</p>
+
+    <h1>Value: ${{ valueLog }}</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Range from "./components/range-input.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    'range-input': Range
+  },
+  data() {
+    return {
+      value: 50
+    };
+  },
+  computed: {
+    valueLog: function() {
+      const position = this.value;
+      // position will be between 0 and 100
+      const minp = 0;
+      const maxp = 100;
+
+      // The result should be between 100 an 10000000
+      const minv = Math.log(100);
+      const maxv = Math.log(100000000000000);
+
+      // calculate adjustment factor
+      const scale = (maxv - minv) / (maxp - minp);
+
+      const val = Math.exp(minv + scale * (position - minp));
+      return (+val.toPrecision(2)).toLocaleString();
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
